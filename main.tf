3d95ce9f774f35daa3554f146d8df48075962ccd
Marcelo Brito <marcelogbrito@gmail.com>
2021-10-12 18:57:44 -0300

    adicionado configuraçao inicial html

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
   ami = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
   key_name = "iac-ubuntu"
+  user_data = <<-EOF
+              #!/bin/bash
+              cd /home/ubuntu
+              echo "<h1>Pagina HTML Configurada com Terraform</h1>" > index.html
+              nohup busybox httpd -f  -p 8080 &
+              EOF
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "Instância AWS provisionada com IAC"
   }
 }
